f04c513c8cc76563506d4626b8ce6b6afcd3c835
Szymon <szymon.osinski.2009@gmail.com>
2025-07-06 13:39:36 +0200

    adjust api_urls for prod

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,6 +1,12 @@
 class ApiUrls {
-  static const baseURL = 'http://10.0.2.2:8080/';
-  static const register = '${baseURL}api/v1/auth/register';
-  static const userProfile = '${baseURL}api/v1/auth/me';
-  static const login = '${baseURL}api/v1/auth/login';
+  static const String _devBaseURL = 'http://10.0.2.2:8080/';
+  static const String _prodBaseURL =
+      'https://edumate-production-d74d.up.railway.app/';
+
+  static const bool _isProduction = true;
+  static String get _baseURL => _isProduction ? _prodBaseURL : _devBaseURL;
+
+  static String get register => '${_baseURL}api/v1/auth/register';
+  static String get login => '${_baseURL}api/v1/auth/login';
+  static String get userProfile => '${_baseURL}api/v1/auth/me';
 }
